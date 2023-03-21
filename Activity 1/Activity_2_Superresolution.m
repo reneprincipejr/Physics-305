6c3868acb4638a8bb4c22393d93b397844dfeb39
Rene L. Principe Jr <59911988+reneprincipejr@users.noreply.github.com>
2023-03-21 18:48:14 +0800

    tested on actual images

--- Activity 1/Activity_2_Superresolution.m
+++ Activity 1/Activity_2_Superresolution.m
@@ -1,5 +1,6 @@
-% Ioriginal = imread('cameraman.tif');
-Ioriginal = imread('cup_blur_2.jpg');
+Ioriginal = imread('car.png');
+Idouble = im2double(Ioriginal);
+% Ioriginal = imread('cup_blur_2.jpg');
 imshow(Ioriginal)
 title('Original Image')
 %% Simulate and Restore Motion Blur Without Noise
@@ -35,24 +36,28 @@ for i = 1:0.1:2
     title('Restored Blurred Image (i = ' + i + ')')
 end
 %% Simulate and Restore Motion Blur and Gaussian Noise
-blurred = imread('cup_blur_2.jpg');
+blurred = imread('car.png');
 noise_mean = 0;
 noise_var = 0.0001;
 blurred_noisy = imnoise(blurred,'gaussian',noise_mean,noise_var);
 imshow(blurred_noisy)
 title('Blurred and Noisy Image')
 %% 
-wnr2 = deconvwnr(blurred_noisy,PSF);
+img = imread('car.png');
+noise_mean = 0;
+noise_var = 0.0001;
+PSF = fspecial('motion',1.9,0);
+wnr2 = deconvwnr(img,PSF);
 imshow(wnr2)
 title('Restoration of Blurred Noisy Image (NSR = 0)')
 %% 
 signal_var = var(Idouble(:));
 NSR = noise_var / signal_var;
-wnr3 = deconvwnr(blurred_noisy,PSF,NSR);
+wnr3 = deconvwnr(img,PSF,NSR);
 imshow(wnr3)
 title('Restoration of Blurred Noisy Image (Estimated NSR)')
 %% Simulate and Restore Motion Blur and 8-Bit Quantization Noise
-blurred_quantized = imfilter(Ioriginal,PSF,'conv','circular');
+blurred_quantized = imfilter(img,PSF,'conv','circular');
 imshow(blurred_quantized)
 title('Blurred Quantized Image')
 %% 
